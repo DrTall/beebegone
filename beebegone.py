a144b434cf9603a853c8f2cc258b3548416756d7
Sean Fellows <fellows@dropbox.com>
2015-02-24 08:02:04 -0800

    Somewhat lazily handle Zeno reminder messages. Fixes #2

--- beebegone.py
+++ beebegone.py
@@ -44,10 +44,16 @@ if credentials is None or credentials.invalid:
       scope='https://www.googleapis.com/auth/gmail.modify'), STORAGE, http=HTTP)
 GMAIL_SERVICE = build('gmail', 'v1', http=credentials.authorize(HTTP))
 
+TODAY = datetime.datetime.today()
+
 # Parse the Beeminder nag email subject line.
 BEEMINDER_SUBJECT_RE = (
     r'(?P<username>\w+)/(?P<goalname>\w+) on ' +
     r'(?P<month>\d\d)/(?P<day>\d\d) \(.*\).*respond with beeminder data')
+
+BEEMINDER_ZENO_SUBJECT_RE = (
+    r'Eep! .* in 0 days for (?P<username>\w+)/(?P<goalname>\w+) \(\$\d+\)'
+)
 
 thread_ids_to_archive = []
 threads = GMAIL_SERVICE.users().threads().list(
@@ -59,15 +65,31 @@ for thread in threads['threads']:
     for header in message['payload']['headers']:
       if header['name'] != 'Subject':
         continue
-      matcher = re.match(BEEMINDER_SUBJECT_RE, header['value'])
-      if not matcher:
-        continue
 
-      print 'Found a beeminder reminder email: %s' % matcher.groupdict()
+      # Holds email subject data regardless of which RE actually matched.
+      # {'re_group_name': 're_group_data'}
+      subject_data = {}
+      matcher = re.match(BEEMINDER_SUBJECT_RE, header['value'])
+      if matcher:
+        subject_data = matcher.groupdict()
+        print 'Found a beeminder reminder email: %s' % subject_data
+      else:
+        matcher = re.match(BEEMINDER_ZENO_SUBJECT_RE, header['value'])
+        if not matcher:
+          continue
+        subject_data = matcher.groupdict()
+        # Assume Zeno polls are from today. Yes, we could inspect the email
+        # itself but that would require work, and this assumption is going
+        # to be ok as long as this script gets run in between when data is
+        # entered and midnight.
+        subject_data.update({'month': TODAY.month, 'day': TODAY.day})
+        print 'Found a beeminder Zeno email: %s' % subject_data
+      del matcher
+
       beeminder_url = (
           'https://www.beeminder.com/api/v1/users/' +
           '%(username)s/goals/%(goalname)s/datapoints.json' %
-          matcher.groupdict())
+          subject_data)
       beeminder_url += '?' + urllib.urlencode(
           {'auth_token':secrets.BEEMINDER_AUTH_TOKEN})
       try:
@@ -85,8 +107,8 @@ for thread in threads['threads']:
           # This is me punting on all the end of year baloney. Would be more
           # proper to get the date (including year) out of the email itself.
           year=beeminder_data_date.year,
-          month=int(matcher.group('month')),
-          day=int(matcher.group('day')))
+          month=int(subject_data['month']),
+          day=int(subject_data['day']))
       # Hack around the bogus year data (see above).
       while gmail_reminder_date > datetime.datetime.now():
         gmail_reminder_date -= datetime.timedelta(weeks=52)
